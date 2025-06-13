8a575ba8e4bd6308fdebab8051f89934178c2a0d
akbar-demandesk <adops@demandesk.com>
2025-06-13 16:46:06 +0530

    changes

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,47 @@
-"use client"
+// "use client"
 
-import Lottie from "lottie-react";
+// import Lottie from "lottie-react";
 
-const AnimationLottie = ({ animationPath, width }) => {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationPath,
-    style: {
-      width: '95%',
-    }
-  };
+// const AnimationLottie = ({ animationPath, width }) => {
+//   const defaultOptions = {
+//     loop: true,
+//     autoplay: true,
+//     animationData: animationPath,
+//     style: {
+//       width: '95%',
+//     }
+//   };
 
+//   return (
+//     <Lottie {...defaultOptions} />
+//   );
+// };
+
+// export default AnimationLottie;
+
+"use client";
+import { motion } from "framer-motion";
+
+const Animation = () => {
   return (
-    <Lottie {...defaultOptions} />
+    <motion.div
+      // style={{ width: 200, height: 200 }}
+      animate={{ y: [0, -20, 0] }}
+      transition={{
+        duration: 2,
+        repeat: Infinity,
+        repeatType: "loop",
+        ease: "easeInOut",
+      }}
+    >
+      <img
+        src="/exp4.png"
+        alt="Experience Icon"
+        style={{ width: "100%", height: "100%", objectFit: "contain" }}
+        className="rounded"
+      />
+    </motion.div>
   );
 };
 
-export default AnimationLottie;
+export default Animation;
--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -1,15 +1,25 @@
 // @flow strict
 
+"use client";
+// import dynamic from "next/dynamic";
 import { experiences } from "@/utils/data/experience";
 import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
-import experience from '../../../assets/lottie/code.json';
+import experience from "../../../assets/lottie/code.json";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
+import Animation from "../../helper/animation-lottie";
+// const AnimationLottie = dynamic(
+//   () => import("@/components/helper/animation-lottie"),
+//   { ssr: false }
+// );
 
 function Experience() {
   return (
-    <div id="experience" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+    <div
+      id="experience"
+      className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
+    >
       <Image
         src="/section.svg"
         alt="Hero"
@@ -32,51 +42,52 @@ function Experience() {
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
             <div className="w-full h-full">
-              <AnimationLottie animationPath={experience} />
+              <Animation />
             </div>
           </div>
 
           <div>
             <div className="flex flex-col gap-6">
-              {
-                experiences.map(experience => (
-                  <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
-                    <div className="p-3 relative">
-                      <Image
-                        src="/blur-23.svg"
-                        alt="Hero"
-                        width={1080}
-                        height={200}
-                        className="absolute bottom-0 opacity-80"
-                      />
-                      <div className="flex justify-center">
-                        <p className="text-xs sm:text-sm text-[#16f2b3]">
-                          {experience.duration}
+              {experiences.map((experience) => (
+                <GlowCard
+                  key={experience.id}
+                  identifier={`experience-${experience.id}`}
+                >
+                  <div className="p-3 relative">
+                    <Image
+                      src="/blur-23.svg"
+                      alt="Hero"
+                      width={1080}
+                      height={200}
+                      className="absolute bottom-0 opacity-80"
+                    />
+                    <div className="flex justify-center">
+                      <p className="text-xs sm:text-sm text-[#16f2b3]">
+                        {experience.duration}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-x-8 px-3 py-5">
+                      <div className="text-violet-500  transition-all duration-300 hover:scale-125">
+                        <BsPersonWorkspace size={36} />
+                      </div>
+                      <div>
+                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                          {experience.title}
+                        </p>
+                        <p className="text-sm sm:text-base">
+                          {experience.company}
                         </p>
                       </div>
-                      <div className="flex items-center gap-x-8 px-3 py-5">
-                        <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                          <BsPersonWorkspace size={36} />
-                        </div>
-                        <div>
-                          <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                            {experience.title}
-                          </p>
-                          <p className="text-sm sm:text-base">
-                            {experience.company}
-                          </p>
-                        </div>
-                      </div>
                     </div>
-                  </GlowCard>
-                ))
-              }
+                  </div>
+                </GlowCard>
+              ))}
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Experience;
